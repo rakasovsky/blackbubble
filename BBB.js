8feb96cea70ab090c488a6ff0adc460d9c383a28
Roma Rakasovsky <rakasovsky1@gmail.com>
2016-11-21 11:38:04 +0200

    1st commit

---
+++ BBB.js
@@ -0,0 +1,17 @@
+/**
+ * Created by mc815 on 07/11/2016.
+ */
+$(".BBB_news").click(function() {
+    $(this).addClass("active");
+});
+
+$(".BBB_main").not(":first").hide();
+
+
+
+
+
+$(".BBB_news").click(function() {
+    $(".BBB_news").removeClass("active").eq($(this).index()).addClass("active");
+    $(".BBB_main div").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
